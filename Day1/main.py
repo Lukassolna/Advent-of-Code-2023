64c7bf08dc5a20953e74644b6a53a10c3c5d5a1a
lukasja <lukasja@kth.se>
2024-02-13 22:39:48 +0100

    begin part 2

--- Day1/main.py
+++ Day1/main.py
@@ -6,3 +6,19 @@ def first():
             intList=[x for x in line if x.isdigit()]
             total+=int((f'{intList[0]}{intList[-1]}'))
     print(total)
+
+def second():
+  dict = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+list = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+
