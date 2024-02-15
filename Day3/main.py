957cf5f1554fb93e35fd402be9fd4fb230abfb82
lukasja <lukasja@kth.se>
2024-02-15 03:52:08 +0100

    firt attempt day3

---
+++ Day3/main.py
@@ -0,0 +1,90 @@
+import pandas as pd
+
+# Initialize an empty list to hold the lists of characters
+array = []
+
+with open('Day3/input.txt', 'r') as file:
+    for line in file:
+        
+        current_line = []
+        for letter in line.strip():  
+            current_line.append(letter)  
+        array.append(current_line)  
+
+def find_grouped_numbers_in_array(array):
+    numbers = []  # List to store found numbers
+    for row in array:
+        current_number = ''  # String to build the current number
+        for char in row:
+            if char.isdigit():  
+                current_number += char
+            else:
+                if current_number:  
+                    numbers.append(current_number)
+                    current_number = ''  # Reset current number for the next group
+        if current_number:  
+            numbers.append(current_number)
+    return numbers
+
+
+
+
+            
+def get_all_numbers():
+    for i in range (len(array)):
+        for y in range (len(array[0])):
+            cur=array[i][y]
+            if cur.isnumeric():
+                print(f'Current number is : {cur}')
+            
+def is_symbol(sym):
+    if sym.isnumeric():
+        return False
+    if sym=='.':
+        return False
+    else:
+        return True
+
+def next_to_symbol(x_pos, y_pos, array):
+    max_x = len(array) - 1
+    max_y = len(array[0]) - 1
+    checks = [
+        (x_pos + 1, y_pos),
+        (x_pos - 1, y_pos),
+        (x_pos, y_pos + 1),
+        (x_pos, y_pos - 1),
+        (x_pos + 1, y_pos + 1),
+        (x_pos - 1, y_pos - 1),
+        (x_pos - 1, y_pos + 1),
+        (x_pos + 1, y_pos - 1)
+    ]
+    for x, y in checks:
+        if 0 <= x <= max_x and 0 <= y <= max_y:
+            if is_symbol(array[x][y]):
+                return True
+    return False
+
+def loop_array(num,array):
+    for i in range (len(array)):
+        for y in range (len(array[0])):
+            cur=array[i][y]
+            if(cur==num):
+                return i,y
+def test():  
+           
+    grouped_numbers = find_grouped_numbers_in_array(array)
+    for number in grouped_numbers:
+        print("new numer")
+        print(number)
+        for num in number:
+            x,y=loop_array(num,array)
+            print("Is found at the coordinates",x,y)
+            next_to=next_to_symbol(x,y,array)
+            print(next_to)
+            if next_to:
+                print(num)
+test()      
+
+
+
+    
