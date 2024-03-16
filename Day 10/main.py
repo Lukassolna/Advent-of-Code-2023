6573141a483c185cc22d8586132f4f10a1501cf0
lukasja <lukasja@kth.se>
2024-03-16 16:36:08 +0100

    ok

--- Day 10/main.py
+++ Day 10/main.py
@@ -26,30 +26,33 @@ def verify(y,x,maxY,maxX):
     return not (x < 0 or x >= maxX or y < 0 or y >= maxY)
     
 def traverse(array, y, x, maxY, maxX,lastmove,startvalue=0):    
+   
+    if x < 0 or x >= maxX or y < 0 or y >= maxY:
+        return None
+        print("outside of the map bro")
+
+    
+        
+    
     cur = array[y][x]
     print(cur)
 
-    if x < 0 or x >= maxX or y < 0 or y >= maxY:
-        print("outside of the map bro")
-        return None
-    if cur == 'S' and startvalue >0: 
+    if cur == 'S' and startvalue != 0:
         return startvalue
-    
-    
-    # Example conditions for moving in different directions
-    if array[y-1][x] in ('|', '7', 'F') and lastmove not in ['s']  :  # North
+
+    if y > 0 and array[y-1][x] in ('|', '7', 'F') and lastmove != 's':  # North
         print("north")
-        return traverse(array, y-1, x, maxY, maxX,'n',startvalue+1)
-    if array[y+1][x] in ('|', 'J', 'L')and lastmove not in ['n']:  # South
+        return traverse(array, y-1, x, maxY, maxX, 'n', startvalue+1)
+    if y < maxY - 1 and array[y+1][x] in ('|', 'J', 'L') and lastmove != 'n':  # South
         print("south")
-        return traverse(array, y+1, x, maxY, maxX,'s',startvalue+1)
-    if array[y][x+1] in ('-', 'J', '7')and lastmove not in ['w']: # East
+        return traverse(array, y+1, x, maxY, maxX, 's', startvalue+1)
+    if x < maxX - 1 and array[y][x+1] in ('-', 'J', '7') and lastmove != 'w': # East
         print("east")
-        return traverse(array, y, x+1, maxY, maxX,'e',startvalue+1)
-    if array[y][x-1] in ('-', 'F', 'L')and lastmove not in ['e']: # West
+        return traverse(array, y, x+1, maxY, maxX, 'e', startvalue+1)
+    if x > 0 and array[y][x-1] in ('-', 'F', 'L') and lastmove != 'e': # West
         print("west")
-        return traverse(array, y, x-1, maxY, maxX,'w',startvalue+1)
-      
+        return traverse(array, y, x-1, maxY, maxX, 'w', startvalue+1)
+    return None
 
 
 def findS(array):
