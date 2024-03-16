b274246ca74c8f3db0c969c44e5b555c5f092e35
lukasja <lukasja@kth.se>
2024-03-16 14:59:18 +0100

    continuatin day 10

--- Day 10/main.py
+++ Day 10/main.py
@@ -5,15 +5,45 @@ def first():
     
     with open('Day 10/input.txt', 'r') as file: 
         array=[]
+        
         total=0
         for index,line in enumerate(file):
+            
             line=line.strip()
             cur=[]
             for char in line:
                 cur.append(char)
             array.append(cur)
+        maxY=len(array)
+        maxX=len(array[0])
+
         y,x=findS(array)
-        print(y,x)
+
+
+        cur=array[y][x]
+        print(traverse(array,y,x,maxY,maxX,'y',0))
+
+def traverse(array, y, x, maxY, maxX,lastmove,startvalue=0):
+    # check 
+    if x < 0 or x >= maxX or y < 0 or y >= maxY:
+        print("outside of the map bro")
+        return None
+       
+        
+    cur = array[y][x]
+    print(cur)
+    if cur == 'S' and startvalue >0: 
+        return startvalue
+    # Example conditions for moving in different directions
+    if array[y-1][x] in ('|', '7', 'F') and lastmove not in ['s']:  # North
+        return traverse(array, y-1, x, maxY, maxX,'n',startvalue+1)
+    elif array[y+1][x] in ('|', 'J', 'L')and lastmove not in ['n']:  # South
+        return traverse(array, y+1, x, maxY, maxX,'s',startvalue+1)
+    elif array[y][x+1] in ('-', 'J', '7')and lastmove not in ['w']: # East
+        return traverse(array, y, x+1, maxY, maxX,'e',startvalue+1)
+    elif array[y][x-1] in ('-', 'F', 'L')and lastmove not in ['e']: # West
+        return traverse(array, y, x-1, maxY, maxX,'w',startvalue+1)
+      
 
 
 def findS(array):
