4535ab3a321e7037c539ea224c680ad6bb3aa27d
lukasja <lukasja@kth.se>
2024-03-22 00:15:51 +0100

    two pointer implemented

--- Day 13/main.py
+++ Day 13/main.py
@@ -1,92 +1,78 @@
-def parse(filename):
-    with open(filename, 'r') as f:
-        lines = [x.strip() for x in f.readlines()]
-    
-    grids = [[]]
-    for line in lines:
-        if line == '':
-            grids.append([])
-            continue
+import re
+import numpy as np
+import sys
+
+def first(): 
+    with open('Day 13/input.txt', 'r') as file: 
+        arrays =[]   
+        array=[]  
+        for line in file:             
+            if line.strip() == '':    
+                arrays.append(array)
+                array=[] 
+                continue
+            
+            line = line.strip()
+            cur = []
+            for char in line:
+                cur.append(char)
+            array.append(cur) 
+        if array:
+            arrays.append(array)              
+        return arrays
+
+def findMiddleColumns(ar):
+    pairs=[]
+    for i in range(len(ar[0])):
+        if i>0:
+            if np.array_equal(ar[:,i], ar[:,i-1]):
+                pairs.append((i,i-1))
+    return pairs
+            
+def findMiddleRows(ar):
+    pairs=[]
+    for i in range(len(ar)):
+        if i>0:
+            if np.array_equal(ar[i], ar[i-1]):
+                pairs.append((i,i-1))
+    return pairs
+
+def two_pointer_col(x_col,y_col,ar): 
+    x=x_col
+    y=y_col
+    while x>=0 and y<len(ar[0]):
+        if not np.array_equal(ar[:,x], ar[:,y]):
+            return 0,0
+        x=x-1
+        y=y+1
+    return x_col,y_col
+
+def two_pointer_row(x_col,y_col,ar):
+    x=x_col
+    y=y_col
+    while x>=0 and y<len(ar):
+        if not np.array_equal(ar[x], ar[y]):
+            return 0,0
+        x=x-1
+        y=y+1
+    return x_col,y_col
+
+def doall():
+    ars=first()
+    total=0
+    for individual_array in ars:
+        ar=np.asarray(individual_array)
+        pairs=findMiddleColumns(ar)    
+        row_pairs=findMiddleRows(ar)
         
-        grids[-1].append(line)
-    
-    return grids
-
-def transpose(grid):
-    return [*zip(*grid)]
-
-def convert_to_int(grid_line):
-    grid_line = ''.join(grid_line).replace('.', '0').replace('#', '1')
-    return int(grid_line, 2)
-
-def find_reflection(grid):
-    encoded_grid = [convert_to_int(line) for line in grid]
-
-    for i in range(1, len(grid)):
-        left = encoded_grid[:i][::-1]
-        right = encoded_grid[i:]
-
-        length = min(len(left), len(right))
-        left = left[:length]
-        right = right[:length]
-
-        if left == right:
-            return i
-
-    return -1
-
-def is_power_of_two(n):
-    return (n & (n-1) == 0) and n != 0
-
-def find_smudge(grid):
-    encoded_grid = [convert_to_int(line) for line in grid]
-
-    for i in range(1, len(grid)):
-        left = encoded_grid[:i][::-1]
-        right = encoded_grid[i:]
-
-        length = min(len(left), len(right))
-        left = left[:length]
-        right = right[:length]
-
-        diff = [l ^ r for l, r in zip(left, right) if l != r]
-        if len(diff) == 1 and is_power_of_two(diff[0]):
-            return i
-
-    return -1
-
-
-def part1(grids):
-    s = 0
-    for grid in grids:
-        guess1 = find_reflection(grid) # rows
-        guess2 = find_reflection(transpose(grid)) # cols
-
-        if guess1 != -1:
-            s += guess1 * 100
-        
-        if guess2 != -1:
-            s += guess2
-        print(s)
-        
-    return s
-
-def part2(grids):
-    s = 0
-
-    for grid in grids:
-        guess1 = find_smudge(grid) # rows
-        guess2 = find_smudge(transpose(grid)) # cols
-
-        if guess1 != -1:
-            s += guess1 * 100
-        
-        if guess2 != -1:
-            s += guess2
-        
-    return s
-
-if __name__ == '__main__':
-    grids = parse('Day 13/input.txt')
-    print(f"Part 1: {part1(grids)}")
-    #print(f"Part 2: {part2(grids)}")
+        for pair in pairs:
+            x_col,y_col=pair
+            x_col,y_col=two_pointer_col(y_col,x_col,ar)
+            total+= (y_col)
+            
+        for pair in row_pairs:
+            x_row,y_row=pair
+            x_row,y_row=two_pointer_row(y_row,x_row,ar)
+            total+= (y_row*100)
+    return total
+print(doall())
